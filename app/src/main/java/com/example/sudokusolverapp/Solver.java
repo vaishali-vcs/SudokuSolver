2fd8b892edadc28968ae718391719424da88a734
vaishali-vcs <vaishali.bhat@yahoo.com>
2021-09-09 20:52:26 -0400

    version with sudoku solver and check board state
    version with sudoku solver and check board state

--- app/src/main/java/com/example/sudokusolverapp/Solver.java
+++ app/src/main/java/com/example/sudokusolverapp/Solver.java
@@ -17,17 +17,13 @@ public class Solver {
         board = new int[9][9];
 
         for(int r=0; r<9;r++)
-        {
-            for(int c=0; c<9;c++)
-            {
+           for(int c=0; c<9;c++)
                 board[r][c] = 0;
-            }
-        }
 
         emptyBoxIndex =new ArrayList<>();
     }
 
-    private void getEmptyBoxIndexs()
+    public void getEmptyBoxIndexes()
     {
         for(int r=0; r<9;r++)
         {
@@ -41,6 +37,75 @@ public class Solver {
                 }
             }
         }
+    }
+
+    private boolean check(int row, int col)
+    {
+        if(this.board[row][col] > 0)
+        {
+            for(int i =0;i<9;i++)
+            {
+                if(this.board[i][col] == this.board[row][col] && row != i)
+                    return false;
+
+                if(this.board[row][i] == this.board[row][col] && col != i)
+                    return false;
+            }
+
+            int boxRow = row/3;
+            int boxCol = col/3;
+
+            for(int r = boxRow*3; r <boxRow *3 +3; r++)
+            {
+                for(int c = boxCol*3; c <boxCol *3 +3; c++)
+                {
+                    if(this.board[r][c] == this.board[row][col] && row != r && col != c)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean solve(SudokuBoard display)
+    {
+        int row = -1;
+        int col = -1;
+
+        for(int r = 0; r< 9; r++)
+            for(int c= 0; c<9;c++)
+              if (this.board[r][c] == 0)
+                {
+                    row = r;
+                    col = c;
+                    break;
+                }
+
+        if(row==-1 || col == -1)
+            return true;
+
+        for(int i = 1; i < 10; i++)
+        {
+            this.board[row][col] = i;
+            display.invalidate();
+
+            if(check(row, col))
+                if(solve(display))
+                    return true;
+
+            this.board[row][col] = 0;
+        }
+
+        return false;
+    }
+
+    public void resetBoard()
+    {
+        for(int r=0; r<9;r++)
+            for(int c=0; c<9;c++)
+                board[r][c] = 0;
+
+        this.emptyBoxIndex =new ArrayList<>();
     }
 
     public void setNumberPos(int num)
